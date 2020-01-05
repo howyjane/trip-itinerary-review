cceef162a93577d293d584713fdfb8066eb2c5dc
Ubuntu <ubuntu@ip-172-31-16-127.us-east-2.compute.internal>
2020-01-05 05:28:03 +0000

    Added search, summary and landingpagesummary html files and amended content in add and index.html

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
-import re
+
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'Project3'
@@ -16,18 +16,34 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/index')
 def index():
-    tripInfo = conn[DATABASE_NAME][COLLECTION_NAME].find()
+    tripInfo = conn[DATABASE_NAME][COLLECTION_NAME].find().limit(8)
     return render_template("index.html", tripInfo=tripInfo)
-    
-    
+
+@app.route('/search_trip')
+def search_trip():
+    return render_template("search.html") 
+
+@app.route('/summary_trip')
+def summary_trip():
+    return render_template("summary.html")  
+
+@app.route('/landingpagesummary_trip')
+def landingpagesummary_trip():
+    return render_template("landingpagesummary.html")  
+
 @app.route('/add_trip')
 def add_trip():
     return render_template("add.html")
 
-
-
-
-
+@app.route('/add_trip', methods=['POST'])
+def getvalue():
+    name = request.form['name']
+    return render_template("summary.html", n=name)
+    
+@app.route('/uploads/<path:filename>', methods=['POST'])
+def save_upload(filename):
+    mongo.save_file(filename, request.files['file'])
+    return redirect(url_for('get_upload', filename=filename))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),port=int(os.environ.get('PORT')),debug=True)
