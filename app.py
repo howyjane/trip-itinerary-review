5906148760acd900e8f76fe9d2abdf49df104294
Ubuntu <ubuntu@ip-172-31-16-127.us-east-2.compute.internal>
2020-01-05 10:01:37 +0000

    added new record to database

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, send_from_directory
 from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
+from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = '/home/ubuntu/environment/uploads'
 app.config["MONGO_DBNAME"] = 'Project3'
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 conn = pymongo.MongoClient('mongodb+srv://root:rOOtUser@clusterjane-dczgp.mongodb.net/test?retryWrites=true&w=majority')
@@ -16,7 +18,7 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/index')
 def index():
-    tripInfo = conn[DATABASE_NAME][COLLECTION_NAME].find().limit(8)
+    tripInfo = conn[DATABASE_NAME][COLLECTION_NAME].find().sort("_id", pymongo.DESCENDING).limit(8)
     return render_template("index.html", tripInfo=tripInfo)
 
 @app.route('/search_trip')
@@ -27,9 +29,10 @@ def search_trip():
 def summary_trip():
     return render_template("summary.html")  
 
-@app.route('/landingpagesummary_trip')
-def landingpagesummary_trip():
-    return render_template("landingpagesummary.html")  
+@app.route('/landingpagesummary_trip/<tripId>')
+def landingpagesummary_trip(tripId):
+    tripInfo = conn[DATABASE_NAME][COLLECTION_NAME].find_one(tripId)
+    return render_template("landingpagesummary.html", tripInfo = tripInfo)  
 
 @app.route('/add_trip')
 def add_trip():
@@ -38,12 +41,29 @@ def add_trip():
 @app.route('/add_trip', methods=['POST'])
 def getvalue():
     name = request.form['name']
+    file = request.files['photo']
+    imageUrl = ''
+    if file.filename != '':
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        imageUrl = url_for('uploaded_file', filename=filename)
+    newTrip = {
+        "name": request.form['name'],
+        "country": 'test country',
+        "city": 'test city',
+        "startdate": '01/12/2019',
+        "enddate": '01/12/2019',
+        "triptitle": 'test trip title',
+        "tripreview": 'review',
+        "totalestimatedcost": 123,
+        "imageURL": imageUrl,
+    }
+    conn[DATABASE_NAME][COLLECTION_NAME].insert_one(newTrip)
     return render_template("summary.html", n=name)
     
-@app.route('/uploads/<path:filename>', methods=['POST'])
-def save_upload(filename):
-    mongo.save_file(filename, request.files['file'])
-    return redirect(url_for('get_upload', filename=filename))
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),port=int(os.environ.get('PORT')),debug=True)
