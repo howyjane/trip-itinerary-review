fea235bc9e1bee57151fbb888ab02267e56dec6d
Ubuntu <ubuntu@ip-172-31-16-127.us-east-2.compute.internal>
2020-01-19 12:57:12 +0000

    finalise script js

--- static/script.js
+++ static/script.js
@@ -11,6 +11,7 @@ document.getElementById("number").onblur =function (){
  document.getElementById("display").value = this.value.replace(/,/g, "")
 }
 
+
 const countriesList = document.getElementById("countries");
 
     function newCountrySelection(event) {
@@ -34,21 +35,5 @@ function initialize(countriesData) {
 
 }
 
-    function compareDates() {
 
-        var Startdate= new ('statedate');
-        var Enddate= new Date ('enddate');
-  
-        if(Startdate>Enddate)
-        {
-        alert= ("start date is greater than end date.");
-        }
-        else if(Enddate>Startdate);
-        {
-        alert= ("End date is greater than start date.");
-     
-        }       
-    }
-    
-    compareDates();
 
